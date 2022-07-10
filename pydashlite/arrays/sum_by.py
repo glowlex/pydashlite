47abecbdbe5cdaabca6225cf2ff13bb436f4a5f6
glowlex <antonioavocado777@gmail.com>
2022-07-10 20:18:07 +0300

    update types

--- pydashlite/arrays/sum_by.py
+++ pydashlite/arrays/sum_by.py
@@ -1,10 +1,10 @@
-from typing import Callable, Iterable, TypeVar
+from typing import Callable, Iterable, Optional, TypeVar
 
 T = TypeVar('T')
 Num = TypeVar('Num', int, float)
 
 
-def sumBy(array: Iterable[T], iteratee: Callable[[T], Num] = None, start: Num = 0) -> Num:
+def sumBy(array: Iterable[T], iteratee: Optional[Callable[[T], Num]] = None, start: Num = 0) -> Num:
     if iteratee is None:
-        return sum([y for y in array], start)
-    return sum([iteratee(y) for y in array], start)
+        return sum(array, start=start)
+    return sum([iteratee(y) for y in array], start=start)
