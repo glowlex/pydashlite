7baf94235f50c88abae11e3a84d5168b643fa263
glowlex <antonioavocado777@gmail.com>
2022-08-14 13:57:54 +0300

    update

--- pydashlite/tools/__init__.py
+++ pydashlite/tools/__init__.py
@@ -4,4 +4,4 @@ from .extract_path_keys import extract_path_keys
 from .get_path_array import get_path_array
 from .is_flat import isFlat
 
-__all__ = ['isIterable', 'NoValue']
+__all__ = ['isIterable', 'NoValue', 'isFlat']
